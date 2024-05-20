9f42c773aa7a9b3e134ab435f940c731e5723c95
mrogers8 <mrogers8@wisc.edu>
2024-05-20 18:00:05 -0400

    Add search yarn and get yarn by id routes

--- api/ravelry.py
+++ api/ravelry.py
@@ -2,15 +2,29 @@ import os
 import requests
 
 endpoints = {
-    'yarn_search': 'https://api.ravelry.com/color_families.json'
+    'color_families': 'https://api.ravelry.com/color_families.json',
+    'search_yarn': 'https://api.ravelry.com/yarns/search.json?query={query}',
+    'get_yarn': 'https://api.ravelry.com/yarns/{yarn_id}.json'
 }
 
 ravelry_username = os.getenv('RAVELRY_USERNAME')
 ravelry_password = os.getenv('RAVELRY_PASSWORD')
+auth = (ravelry_username, ravelry_password)
 
-def get_yarn():
-    auth = (ravelry_username, ravelry_password)
-    response = requests.get(endpoints['yarn_search'], auth=auth)
+
+def ravelry_get_color_families():
+    response = requests.get(endpoints['color_families'], auth=auth)
     response.close()
     return response.text
 
+
+def ravelry_search_yarn(query: str):
+    response = requests.get(endpoints['search_yarn'].format(query = query), auth=auth)
+    response.close()
+    return response.text
+
+
+def ravelry_get_yarn(id: int):
+    response = requests.get(endpoints['get_yarn'].format(yarn_id = id), auth=auth)
+    response.close()
+    return response.text
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+import json
 
-from api.ravelry import get_yarn
+from api.ravelry import ravelry_search_yarn, ravelry_get_yarn
 
 load_dotenv()
 
@@ -11,6 +12,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/yarns/search/")
-async def search_yarn():
-    return get_yarn()
+@app.get("/yarns/search/{query}")
+async def search_yarn(query: str):
+    result = ravelry_search_yarn(query)
+    return json.loads(result)
+
+@app.get("/yarns/get/{id}")
+async def search_yarn(id: str):
+    result = ravelry_get_yarn(id)
+    return json.loads(result)
