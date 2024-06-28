2dd1e382fd87e7bd4a48c6bc050e5b3a85468349
mrogers8 <mrogers8@wisc.edu>
2024-06-28 14:10:35 -0400

    Add create stash initial implementation
    Created list stash and create stash APIs. Create stash will currently create stash, but needs debugging on the pack params and yarn_id which is not currently uploading. This base implementation does create a stash entry in the user account

--- api/ravelry.py
+++ api/ravelry.py
@@ -1,3 +1,4 @@
+import json
 import os
 import requests
 
@@ -5,7 +6,9 @@ endpoints = {
     'color_families': 'https://api.ravelry.com/color_families.json',
     'search_yarn': 'https://api.ravelry.com/yarns/search.json?query={query}',
     'get_yarn': 'https://api.ravelry.com/yarns/{yarn_id}.json',
-    'list_stash': 'https://api.ravelry.com/people/{username}/stash/list.json'
+    'list_stash': 'https://api.ravelry.com/people/{username}/stash/{stash_id}.json',
+    'create_stash': 'https://api.ravelry.com/people/{username}/stash/create.json',
+    'update_pack': 'https://api.ravelry.com/packs/{pack_id}.json'
 }
 
 ravelry_username = os.getenv('RAVELRY_USERNAME')
@@ -33,7 +36,39 @@ def ravelry_get_yarn(id: int):
     return response.text
 
 
-def ravelry_list_stash():
-    response = requests.get(endpoints['list_stash'].format(username = ravelry_user), auth=auth)
+def ravelry_list_stash(id: str):
+    response = requests.get(endpoints['list_stash'].format(username = ravelry_user, stash_id = id), auth=auth)
     response.close()
+    return response.text
+
+
+def ravelry_create_stash():
+    stash_params = {
+        "yarn_id": 209189,
+        "Pack": {
+            "colorway": 8080,
+            "skeins": 2,
+            "purchased_date": "1997-08-01",
+            "total_paid": "17.38",
+            "total_paid_currency": "USD"
+        }
+    }
+
+    response = requests.post(endpoints['create_stash'].format(username = ravelry_user), auth=auth,
+                             params=json.dumps(stash_params))
+    response.close()
+    return response.text
+
+
+def ravelry_update_pack(pack_id: str):
+    pack_params = {
+        "colorway": 8080,
+        "skeins": 2,
+        "purchased_date": "1997-08-01",
+        "total_paid": "17.38",
+        "total_paid_currency": "USD"
+    }
+
+    response = requests.put(endpoints['update_pack'].format(pack_id = pack_id), auth=auth,
+                            params=json.dumps(pack_params))
     return response.text
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 import json
 
-from api.ravelry import ravelry_search_yarn, ravelry_get_yarn, ravelry_list_stash
+from api.ravelry import ravelry_search_yarn, ravelry_get_yarn, ravelry_list_stash, ravelry_create_stash, ravelry_update_pack
 
 load_dotenv()
 
@@ -24,7 +24,17 @@ async def search_yarn(id: str):
     return json.loads(result)
 
 
-@app.get("/stash/list")
-async def list_stash():
-    result = ravelry_list_stash()
+#stash_id can also be "list"
+@app.get("/stash/get/{stash_id}")
+async def list_stash(stash_id: str):
+    result = ravelry_list_stash(stash_id)
     return json.loads(result)
+
+
+@app.get("/stash/create")
+async def create_stash():
+    result = ravelry_create_stash()
+    stash_map = json.loads(result)
+    print("STASH SUCCESSFUL: {fstash}".format(fstash=stash_map))
+
+    return json.loads(ravelry_list_stash(stash_map['stash']['packs'][0]['stash_id']))
